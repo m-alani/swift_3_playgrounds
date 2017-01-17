1af2aea2125b587bc66ba82378434d82b229b0b4
Marwan Alani <marwan@alani.ca>
2017-01-17 16:35:43 -0500

    Initial XC project structure

---
+++ My Playground/My Playground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  My Playground
+//
+//  Created by Marwan Alani on 2017-01-17.
+//  Copyright © 2017 Marwan Alani. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
