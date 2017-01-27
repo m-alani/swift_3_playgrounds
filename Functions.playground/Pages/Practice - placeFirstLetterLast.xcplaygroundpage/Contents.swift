2b8e7ad2d88b7f39bcfdc13ec4bdb5ea94be23c4
Marwan Alani <marwan@alani.ca>
2017-01-27 13:03:18 -0500

    Solving practice problem: Funny Strings

--- Functions.playground/Pages/Practice - placeFirstLetterLast.xcplaygroundpage/Contents.swift
+++ Functions.playground/Pages/Practice - placeFirstLetterLast.xcplaygroundpage/Contents.swift
@@ -3,6 +3,6 @@ import Foundation
 
 // TODO: Implement placeFirstLetterLast() here!
 func placeFirstLetterLast(of input: String) -> String {
-    return input[input.endIndex] + input.substring(from: input.startIndex + 1)
+    return input[input.endIndex] + input.substring(from: input.startIndex e+ 1)
 }
 //: [Next](@next)
--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -10,18 +10,31 @@
 import Foundation
 
 // Everything Else
-var input:String?
-print("Please enter a test string: ")
-input = readLine()
-guard var input = input else {
-    print("No test string found")
-    exit(1)
-}
-let fileName = "output.txt"//Bundle.main.bundlePath + "/output.txt"
-do{
-    input.append("\n")
-    try input.write(toFile: fileName, atomically: true, encoding: .ascii)
-    print("Written to file @ \(fileName)")
-} catch{
-    print("Couldn't write shit")
+if let cases = Int(readLine() ?? "0") {
+    var output = [String]()
+    
+    // Process Input
+    for _ in 1...cases {
+        if let line = readLine() {
+            var funny = true
+            let count = line.characters.count
+            for offset in 0...(count/2) - 1 {
+                let myScalars = line.unicodeScalars
+                let firstFrontAscii = Int(myScalars[myScalars.index(myScalars.startIndex, offsetBy: offset)].value)
+                let secondFrontAscii = Int(myScalars[myScalars.index(myScalars.startIndex, offsetBy: offset+1)].value)
+                let firstBackAscii = Int(myScalars[myScalars.index(myScalars.startIndex, offsetBy: (count-1) - offset)].value)
+                let secondBackAscii = Int(myScalars[myScalars.index(myScalars.startIndex, offsetBy: (count-2) - offset)].value)
+                    if (abs(firstFrontAscii - secondFrontAscii) != abs(firstBackAscii - secondBackAscii)) {
+                    funny = false;
+                    break
+                }
+            }
+            output.append(funny ? "Funny" : "Not Funny")
+        }
+    }
+    
+    // Print Output
+    for line in output {
+        print(line)
+    }
 }
