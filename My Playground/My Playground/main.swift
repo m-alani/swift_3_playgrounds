46f80c4ccbb6102f4d9a7fd0efe95d375bfd923a
Marwan Alani <marwan@alani.ca>
2017-01-29 17:55:10 -0500

    Solved HackerRank - Game of Throne I

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -10,22 +10,19 @@ import Foundation
 
 // Read Input
 if let input = readLine() {
-  var charCountSet: Set<Character> = Set()
+  var oddChars: Set<Character> = Set()
   let word = input.characters
   // Process the input line one character at a time. Maintain a set of all characters with odd occurences in the input
   for char in word {
-    if let exists = charCountSet.remove(char) {}
+    if let exists = oddChars.remove(char) {}
     else {
-      charCountSet.insert(char)
+      oddChars.insert(char)
     }
   }
-  // Check for passing conditions: 
-  //   if the input length was even & we had even occurences of all characters
-  //    OR
-  //   if the input length was odd & we had even occurences of all characters except for one
-  if (word.count % 2 == 0 && charCountSet.count == 0) || (word.count % 2 == 1 && charCountSet.count == 1) {
-    print("YES")
-  } else {
-    print("NO")
-  }
+  // Check for passing condition:
+  //   We can have 1 character at most with odd occurences
+  let output = oddChars.count < 2 ? "YES" : "NO"
+  
+  // Print Output
+  print(output)
 }
