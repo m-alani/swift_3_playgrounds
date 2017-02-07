2ae70ee798068107a1654733ab16204222fdb7b4
Marwan Alani <marwan@alani.ca>
2017-02-07 15:29:53 -0500

    Practice problem from HackerRank

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,28 +6,14 @@
 //  Copyright © 2017 Marwan Alani. All rights reserved.
 //
 
-// import Foundation
+import Foundation
 
-if let cases = Int(readLine() ?? "0") {
-  var output = [String]()
-  
-  // Process Input
-  for _ in 1...cases {
-    if let line: String = readLine() {
-      var operations = 0
-      let word = line.utf8
-      let count = word.count
-      for offset in 0...(count/2) - 1 {
-        let frontAscii = Int(word[word.index(word.startIndex, offsetBy: offset)])
-        let backAscii = Int(word[word.index(word.startIndex, offsetBy: (count-1) - offset)])
-        operations += abs(frontAscii - backAscii)
-      }
-      output.append("\(operations)")
-    }
-  }
-  
-  // Print Output
-  for line in output {
-    print(line)
-  }
-}
+// Read the string
+let s = readLine()!
+
+let tempHours = Int(s.substring(with: <#T##Range<String.Index>#>))
+let minutes = Int(s.substring(with: <#T##Range<String.Index>#>))
+let seconds = Int(s.substring(with: <#T##Range<String.Index>#>))
+
+
+
