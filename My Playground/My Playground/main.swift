be29fe281fb2c3d6e8f4c976c78c16f50b7e8886
Marwan Alani <marwan@alani.ca>
2017-02-19 15:40:38 -0500

    Bubble sort practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,19 +8,35 @@
 
 import Foundation
 
-// Read n
-let n = Int(readLine() ?? "0")!
-
-// Read the array
-var numbers = String((readLine() ?? "0")!).components(separatedBy: " ").map({Int($0) ?? 0}).sorted()
-
-// Process the output
-var numberOfCuts = numbers.count
-while numberOfCuts != 0 {
-    print(numberOfCuts)
-    let deletedNumber = numbers.removeFirst()
-    while numbers.first ?? deletedNumber + 1 == deletedNumber {
-        numbers.removeFirst()
+// Definition of the generic Bubble Sort Algorithm function
+func bubbleSort<T>(inputArray: [T], comparator:(T,T) -> Bool) -> [T] {
+    var outputArray = inputArray
+    if outputArray.count > 1 {
+        for mainIndex in 0...outputArray.count-2 {
+            for subIndex in 0...outputArray.count-mainIndex-2 {
+                if comparator(outputArray[subIndex], outputArray[subIndex+1]) {
+                    swap(&outputArray[subIndex], &outputArray[subIndex+1])
+                }
+            }
+        }
     }
-    numberOfCuts = numbers.count
+    return outputArray
+}
+
+// Definition of generic comparator functions
+func ascending<Type: Comparable> (firstItem: Type, secondItem: Type) -> Bool {return firstItem > secondItem}
+func descending<Type: Comparable> (firstItem: Type, secondItem: Type) -> Bool {return firstItem < secondItem}
+
+// Setting the size of the test array (and the upper bound of the random numbers to fill it: 1 to N)
+let N: UInt32 = 100
+
+// Initialization of the test array
+var unsortedArray = (1...N).map{_ in Int(arc4random_uniform(N)+1)}
+
+// Sorting the array without mutating it (change the comparator function to control the order of the sort)
+let sortedArray = bubbleSort(inputArray: unsortedArray, comparator: ascending)
+
+// Printing the sorted array
+for number in sortedArray {
+    print(number)
 }
