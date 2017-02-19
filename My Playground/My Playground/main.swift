adc2a64fb9c8363c08e51ed141d044cf15a7a345
Marwan Alani <marwan@alani.ca>
2017-02-19 16:12:58 -0500

    optimized bubble sort

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,35 +8,45 @@
 
 import Foundation
 
-// Definition of the generic Bubble Sort Algorithm function
+// Definition of the generic Bubble Sort Algorithm function (optimized)
 func bubbleSort<T>(inputArray: [T], comparator:(T,T) -> Bool) -> [T] {
     var outputArray = inputArray
+    var iterations = 0
+    var swaps = 0
     if outputArray.count > 1 {
         for mainIndex in 0...outputArray.count-2 {
+            var noSwapsOccured = true
             for subIndex in 0...outputArray.count-mainIndex-2 {
                 if comparator(outputArray[subIndex], outputArray[subIndex+1]) {
                     swap(&outputArray[subIndex], &outputArray[subIndex+1])
+                    swaps += 1
+                    noSwapsOccured = false
                 }
+            }
+            iterations += 1
+            // Check if the array is already sorted
+            if noSwapsOccured {
+                break
             }
         }
     }
+    print("Bubble Sort completed after \(swaps) swap(s) thourgh \(iterations) iteration(s)")
     return outputArray
 }
 
 // Definition of generic comparator functions
-func ascending<Type: Comparable> (firstItem: Type, secondItem: Type) -> Bool {return firstItem > secondItem}
-func descending<Type: Comparable> (firstItem: Type, secondItem: Type) -> Bool {return firstItem < secondItem}
+func ascending<T: Comparable> (firstItem: T, secondItem: T) -> Bool {return firstItem > secondItem}
+func descending<T: Comparable> (firstItem: T, secondItem: T) -> Bool {return firstItem < secondItem}
 
-// Setting the size of the test array (and the upper bound of the random numbers to fill it: 1 to N)
-let N: UInt32 = 100
+// Set the size of the test array (and the upper bound of the random numbers to fill it: 1 to N)
+let N: UInt32 = 20
 
-// Initialization of the test array
+// Initialize (and print) the test array
 var unsortedArray = (1...N).map{_ in Int(arc4random_uniform(N)+1)}
+print(unsortedArray)
 
-// Sorting the array without mutating it (change the comparator function to control the order of the sort)
+// Sort the array without mutating it (change the comparator function to control the order of the sort)
 let sortedArray = bubbleSort(inputArray: unsortedArray, comparator: ascending)
 
-// Printing the sorted array
-for number in sortedArray {
-    print(number)
-}
+// Print the sorted array
+print(sortedArray)
