67e94d88cb6358756675b2369de959a301d2a778
Marwan Alani <marwan@alani.ca>
2017-02-22 13:55:21 -0500

    Simple Linked List fun

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,30 +8,127 @@
 
 import Foundation
 
-// Reading the number of test cases
-let cases = Int(readLine() ?? "0") ?? 0
-
-// Processing the test cases
-for _ in 0..<cases {
-  var output = "NO"
-  var left = 0
-  // The number of elements in the array doesn't matter in our implementation, but we need to clear that line from the input buffer
-  let caseLength = Int(readLine() ?? "0") ?? 0
-  // Read the test case into an array of strings (Swift readline() limitation)
-  let inputString = String(readLine() ?? "")!.components(separatedBy: " ")
-  // Convert the strings into an array of integers
-  let input = inputString.map({Int($0) ?? 0})
-  // Find the sum of the array
-  let sum = input.reduce(0, {$0 + $1})
-  // Process the array to find the requested index where the sum of its left is equal to the sum of its right (if any)
-  for number in input {
-    if ((sum-number)/2 == left) {
-      output = "YES"
-      break
-    } else {
-      left += number
+/// Base Node class to be used in a Linked List
+class Node<T> {
+    
+    /// The variable to hold the data part of the node
+    let data: T
+    
+    /// An optional link to the next node
+    var next: Node<T>?
+    
+    init(data: T) {
+        self.data = data
+        self.next = nil
     }
-  }
-  // Print the output for this test case
-  print(output)
+    
+    init(data: T, nextNode: Node<T>?) {
+        self.data = data
+        self.next = nextNode
+    }
 }
+
+/// Generic Singly Linked List implementation
+class LinkedList<T> {
+    
+    private var root: Node<T>?
+    
+    /// The number of nodes/values in the list.
+    public var count: Int = 0
+    
+    /// Default initializer with empty list
+    init() {
+        self.root = nil
+    }
+    
+    /// Overloaded initializer to create a list with 1 node
+    ///
+    /// - Parameter root: Set the root of the list as the passed parameter
+    init(root r: Node<T>) {
+        self.root = r
+    }
+    
+    
+    /// Create a string representation of the list
+    ///
+    /// - Returns: A string conatining the comma separated values of the list
+    public func toString() -> String {
+        var tempString = "["
+        var currentNode = self.root
+        if let unwrappedNode = currentNode {
+            tempString += "\(unwrappedNode.data)"
+            currentNode = unwrappedNode.next
+        }
+        while (currentNode != nil) {
+            tempString += ", \(currentNode!.data)"
+            currentNode = currentNode!.next
+        }
+        tempString += "]"
+        return tempString
+    }
+    
+    
+    /// Check if the list is empty or not
+    ///
+    /// - Returns: A boolean value to indicate whether the list is empty.
+    public func isEmpty() -> Bool {
+        return self.count == 0
+    }
+    
+    
+    /// Inserts a value at the beginning of the list. O(1)
+    ///
+    /// - Parameter data: The data value to be inserted
+    public func insertFirst(data: T) {
+        let newNode = Node(data: data, nextNode: self.root)
+        self.root = newNode
+    }
+    
+    
+    /// Inserts a node at the beginning of the list. O(1)
+    ///
+    /// - Parameter node: The node to be inserted
+    public func insertFirst(node: Node<T>) {
+        node.next = self.root
+        self.root = node
+    }
+    
+    
+    /// Removes the first node in the list and returns its value. If the list is empty, it returns a nil.
+    ///
+    /// - Returns: An optional T. The data of the removed node (if any), or nil otherwise
+    public func removeFirst() -> T? {
+        var removedValue: T? = nil
+        if (!self.isEmpty()) {
+            removedValue = root?.data
+            self.root = self.root!.next
+        }
+        return removedValue
+    }
+    
+    
+    /// Inserts a value at the end of the list. O(n)
+    ///
+    /// - Parameter data: The data value to be inserted
+    public func insertLast(data: T) {
+        if (self.count == 0) {
+            self.root = Node(data: data)
+            self.count += 1
+        } else {
+            var currentNode = self.root
+            while (currentNode!.next != nil) {
+                currentNode = currentNode!.next
+            }
+            currentNode!.next = Node(data: data)
+            self.count += 1
+        }
+    }
+}
+
+
+
+let test = LinkedList<Int>()
+test.insertFirst(data: 2)
+test.insertFirst(data: 1)
+
+print(test.toString())
