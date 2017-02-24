8e4688b87912ab1bafbf80957737d6b4975389ff
Marwan Alani <marwan@alani.ca>
2017-02-24 14:57:34 -0500

    Experimenting with stacks

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -10,7 +10,7 @@ import Foundation
 
 //************ Node Class Begins ************//
 
-/// Base Node class to be used in a Linked List
+/// Base Node class to be used in a Stack
 class Node<T> {
     
     /// The variable to hold the data part of the node
@@ -32,172 +32,84 @@ class Node<T> {
 
 //************ Node Class Ends ************//
 
-//************ Linked List Class Begins ************//
+//************ Stack Class Begins ************//
 
-/// Generic Singly Linked List implementation
-class LinkedList<T> {
+/// Generic Stack implementation
+class Stack<T> {
     
-    /// The root node of the list
-    private var root: Node<T>?
+    /// The top node of the list
+    private var top: Node<T>?
     
-    /// The number of nodes/values in the list.
-    public var count: Int = 0
-    
-    /// Default initializer with empty list
+    /// Default initializer with empty stack
     init() {
-        self.root = nil
+        self.top = nil
     }
     
-    /// Overloaded initializer to create a list with 1 node
+    /// Overloaded initializer to create a stack with 1 node
+    /// Note that the "node" is passed by reference, hence we copy its data to a new node first
     ///
-    /// - Parameter root: Set the root of the list as the passed parameter
-    init(root r: Node<T>) {
-        self.root = r
+    /// - Parameter top: Set the top of the stack as the passed parameter
+    init(top: Node<T>) {
+        let nodeCopy = Node(data: top.data)
+        self.top = nodeCopy
     }
     
-    /// Create a string representation of the list
+    /// Check if the stack is empty
     ///
-    /// - Returns: A string conatining the comma separated values of the list
-    public func toString() -> String {
-        var tempString = "["
-        var currentNode = self.root
-        if let unwrappedNode = currentNode {
-            tempString += "\(unwrappedNode.data)"
-            currentNode = unwrappedNode.next
-        }
-        while (currentNode != nil) {
-            tempString += ", \(currentNode!.data)"
-            currentNode = currentNode!.next
-        }
-        tempString += "]"
-        return tempString
-    }
-    
-    /// Check if the list is empty
-    ///
-    /// - Returns: A boolean value to indicate whether the list is empty.
+    /// - Returns: A boolean value to indicate whether the stack is empty.
     public func isEmpty() -> Bool {
-        return self.count == 0
+        return self.top == nil
     }
     
-    /// Inserts a value at the beginning of the list. O(1)
+    /// Inserts a value at the top of the stack. O(1)
     ///
     /// - Parameter data: The data value to be inserted
-    public func insertFirst(data: T) {
-        let newNode = Node(data: data, nextNode: self.root)
-        self.root = newNode
-        self.count += 1
+    public func push(data: T) {
+        let newNode = Node(data: data, nextNode: self.top)
+        self.top = newNode
     }
     
-    /// Inserts a node at the beginning of the list. O(1)
+    /// Inserts a node at the top of the stack. O(1)
+    /// Note that the "node" is passed by reference, hence we copy its data to a new node first
     ///
     /// - Parameter node: The node to be inserted
-    public func insertFirst(node: Node<T>) {
-        node.next = self.root
-        self.root = node
-        self.count += 1
+    public func push(node: Node<T>) {
+        let nodeCopy = Node(data: node.data)
+        nodeCopy.next = self.top
+        self.top = nodeCopy
     }
     
-    /// Inserts a value at the end of the list. O(n)
-    ///
-    /// - Parameter data: The data value to be inserted
-    public func insertLast(data: T) {
-        if (self.count == 0) {
-            self.root = Node(data: data)
-            self.count += 1
-        } else {
-            var currentNode = self.root
-            while (currentNode!.next != nil) {
-                currentNode = currentNode!.next
-            }
-            currentNode!.next = Node(data: data)
-            self.count += 1
-        }
-    }
     
-    /// Inserts a node at the end of the list. O(n)
-    ///
-    /// - Parameter node: The node to be inserted
-    public func insertLast(node: Node<T>) {
-        if (self.count == 0) {
-            self.root = node
-            self.count += 1
-        } else {
-            var currentNode = self.root
-            while (currentNode!.next != nil) {
-                currentNode = currentNode!.next
-            }
-            currentNode!.next = node
-            self.count += 1
-        }
-    }
-    
-    /// Removes the first node in the list and returns its value. If the list is empty, it returns a nil. O(1)
+    /// Removes the top node in the stack and returns its value. If the stack is empty, it returns a nil. O(1)
     ///
     /// - Returns: An optional T. The data of the removed node (if any), or nil otherwise
-    public func removeFirst() -> T? {
+    public func pop() -> T? {
         var removedValue: T? = nil
-        if (self.count != 0) {
-            removedValue = root?.data
-            self.root = self.root!.next
-            self.count -= 1
+        if (self.top != nil) {
+            removedValue = self.top?.data
+            self.top = self.top!.next
         }
         return removedValue
     }
 
-    /// Removes the last node in the list and returns its value. If the list is empty, it returns a nil. O(n)
-    ///
-    /// - Returns: An optional T. The data of the removed node (if any), or nil otherwise
-    public func removeLast() -> T? {
-        var removedValue: T? = nil
-        if (self.count == 1) {
-            removedValue = self.root!.data
-            self.root = nil
-            self.count -= 1
-        } else if (self.count > 1) {
-            var previousNode = self.root
-            while (previousNode!.next!.next != nil) {
-                previousNode = previousNode!.next
-            }
-            removedValue = previousNode!.next!.data
-            previousNode!.next = nil
-            self.count -= 1
-        }
-        return removedValue
-    }
     
-    /// Returns the first data value in the list (if any). O(1)
+    /// Returns the top data value in the stack (if any) without deleting the node. O(1)
     ///
-    /// - Returns: The optional data value of the first node in the list. Returns nil if the list is empty
-    public func first() -> T? {
+    /// - Returns: The optional data value of the first node in the stack. Returns nil if the stack is empty
+    public func peek() -> T? {
         var value: T? = nil
-        if (self.count > 0) {
-            value = self.root?.data
+        if (self.top != nil) {
+            value = self.top!.data
         }
         return value
     }
     
-    /// Returns the last data value in the list (if any). O(n)
-    ///
-    /// - Returns: The optional data value of the last node in the list. Returns nil if the list is empty
-    public func last() -> T? {
-        var value: T? = nil
-        if (self.count > 0) {
-            var currentNode = self.root
-            while (currentNode?.next != nil) {
-                currentNode = currentNode?.next
-            }
-            value = currentNode?.data
-        }
-        return value
-    }
-    
-    /// Create an Array from the contents of the list. O(n)
+    /// Create an Array from the contents of the stack. O(n)
     ///
     /// - Returns: An array of type T containing the elements of the list in the same order
     public func toArray() -> [T] {
         var array = [T]()
-        var currentNode = self.root
+        var currentNode = self.top
         while (currentNode != nil) {
             array.append(currentNode!.data)
             currentNode = currentNode!.next
@@ -206,38 +118,32 @@ class LinkedList<T> {
     }
 }
 
-//************ Linked List Class Ends ************//
+//************ Stack Class Ends ************//
 
 //************ Main Program (for testing) Begins ************//
 
 // Create an empty list
-let myList = LinkedList<Int>()
+let myStack = Stack<Int>()
 
 // Set N to the desired number of test elements. Default is 20
 let N: UInt32 = 20
 
-// Populate the list with random numbers (half to the front, half to the back)
-for _ in 1...N/2 {
-    myList.insertFirst(data: Int(arc4random_uniform(N)+1))
-}
-for _ in 1...N/2 {
-    myList.insertLast(data: Int(arc4random_uniform(N)+1))
+// Populate the stack with random numbers
+print("Pushing \(N) random number(s) into the Stack...\n")
+for _ in 1...N {
+    myStack.push(data: Int(arc4random_uniform(N)+1))
 }
 
-// Print the list
-print("Initial List with \(myList.count) item(s):")
-print(myList.toString(),"\n")
+// Print the stack as an Array
+print("Printing the Stack as an Array:")
+print(myStack.toArray(),"\n")
 
-// Use each remove function once
-print("Removeing the first element: \(myList.removeFirst()!)")
-print("Removeing the last element: \(myList.removeLast()!)\n")
+// Remove the first 3 values
+print("Popping the top element: \(myStack.pop()!)")
+print("Popping the top element: \(myStack.pop()!)")
+print("Popping the top element: \(myStack.pop()!)\n")
 
-// Use the "peek" functions (first & last)
-print("Peeking into the first element: \(myList.first()!)")
-print("Peeking into the last element: \(myList.last()!)\n")
-
-// Printing the list as an array
-print("The list as an array of \(myList.count) item(s):")
-print(myList.toArray())
+// Peek at the current top
+print("Peeking into the top element of the Stack: \(myStack.peek()!)\n")
 
 //************ Main Program (for testing) Ends ************//
