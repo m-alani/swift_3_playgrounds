19ec7ef7f5bf3373054c1483b9242b7334eff79b
Marwan Alani <marwan@alani.ca>
2017-03-30 15:25:45 -0400

    More algorithms practice from HackerRank

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,31 +8,20 @@
 
 import Foundation
 
-// Helper function to check if an integer is a factor of another
-func isFactorOf(number x: Int, factorOf y: Int) -> Bool {
-    return y % x == 0
-}
-
-// Read (and ignore) N & M
+// Read (and ignore) N
 _ = readLine()
 
-// Read A & B
-let arrayA = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!}).sorted()
-let arrayB = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!}).sorted()
+// Read the candles heights
+let heights = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!})
 
-// Process the arrays
+// Process the case
 var output = 0
-if (arrayA[arrayA.count-1] < arrayB[0]) {
-    xCheck: for x in arrayA[arrayA.count-1]...arrayB[0] {
-        var satisfies = true
-        for a in arrayA {
-            satisfies = isFactorOf(number: a, factorOf: x)
-            if (!satisfies) { continue xCheck}
-        }
-        for b in arrayB {
-            satisfies = isFactorOf(number: x, factorOf: b)
-            if (!satisfies) { continue xCheck}
-        }
+var max = heights[0]
+for height in heights {
+    if height > max {
+        max = height
+        output = 1
+    } else if height == max {
         output += 1
     }
 }
