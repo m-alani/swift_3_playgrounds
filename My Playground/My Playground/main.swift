f4231d020872f3c75671fb6109285c7a684a4db0
Marwan Alani <marwan@alani.ca>
2017-04-01 16:31:03 -0400

    Practicing HackerRank algorithms track

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,21 +8,24 @@
 
 import Foundation
 
-// Read K
-let k = Int(String(readLine()!)!.components(separatedBy: " ")[1])!
+// Read N (and ignore it, as it is not needed in this implementation)
+_ = readLine()
 
-// Read the array
-let array = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!})
+// Read the scores
+let birds = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!})
 
-// Process the array
+// Process the scores
+var count = [0,0,0,0,0]
+for bird in birds {
+    count[bird-1] += 1
+}
 var output = 0
-for i in 0..<array.count-1 {
-    for j in i+1..<array.count {
-        if (array[i] + array[j]) % k == 0 {
-            output += 1
-        }
+for type in 1...4 {
+    if count[type] > count[output] {
+        output = type
     }
 }
+output += 1 // Make up for the starting index difference (array starts at 0, bird types at 1)
 
 // Print the output
 print(output)
