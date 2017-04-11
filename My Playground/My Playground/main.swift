2666bdbbf348dcf9f6ceceae2e980e692899b5d8
Marwan Alani <marwan@alani.ca>
2017-04-11 16:51:16 -0400

    Solving a contest challenge from HackerRank

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -16,7 +16,31 @@ var output = [String]()
 for _ in 1...queries {
   _ = readLine()
   var array = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!})
-  var unswappable = false
-  var sorted = false
-  
+  var unswappable = false   // Flag to break out when we can't perform a swap
+  var sorted = false        // Flag to break out if a single search cycle is completed with no swaps (the array is sorted already)
+  main:for i in 1...array.count {
+    var j = array.count - i
+    sorted = true
+    while (j > 0) {
+      if (array[j] < array[j-1]) {
+        if (abs(array[j] - array[j-1]) > 1) {
+          unswappable = true
+          break main
+        } else {
+          swap(&array[j], &array[j-1])
+          sorted = false
+        }
+      }
+      j -= 1
+    }
+    if (sorted) {
+      break main
+    }
+  }
+  output.append((unswappable) ? "No" : "Yes")
+}
+
+// Print the output
+for answer in output {
+  print(answer)
 }
