0ccc150268f4740da2294c07a9f31174442747ca
Marwan Alani <marwan@alani.ca>
2017-06-13 09:24:29 -0400

    HackerRank practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,29 +8,16 @@
 
 import Foundation
 
-// Helper function to find the reverse of an Int
-func reverse(_ number: Int) -> Int {
-  var reversed: Int = 0
-  var current = number
-  while current != 0 {
-    reversed = reversed * 10 + current % 10
-    current = current / 10
-  }
-  return reversed
-}
+// Read N
+let days = Int(readLine()!)!
 
-// Read input
-let input = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!})
-
-// Process the case
-var output = 0
-for i in input[0]...input[1] {
-  var current = i
-  var reversed = reverse(i)
-  if ((i - reversed) % input[2] == 0) {
-    output += 1
-  }
+// Find the number of likes
+var base = 2
+var likes = 2
+for _ in 1..<days {
+  base = (base * 3) / 2
+  likes += base
 }
 
 // Print the output
-print(output)
+print(likes)
