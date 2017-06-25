bd1e0a0ef4a4255b145bc6e44ea30e93e078383c
Marwan Alani <marwan@alani.ca>
2017-06-25 11:32:36 -0400

    Working on a HackerRank problem

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,3 +8,11 @@
 
 import Foundation
 
+// Read the input
+_ = readLine()
+let input = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!})
+
+// Find & print the output
+for i in 0..<input.count {
+  print(input.index(of: input.index(of: (i+1))! + 1)! + 1)
+}
