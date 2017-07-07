e188687f4e2ca5753cba1a8c2d2ad17c0b220a09
Marwan Alani <marwan@alani.ca>
2017-07-07 09:25:00 -0400

    Finally back to HackerRank practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,50 +8,19 @@
 
 import Foundation
 
-// Define a data structure to hold required info for a single violation category
-struct Stats {
-  var count: Int
-  var earliestOccurance: Date
-  var latestOccurance: Date
-}
+// Read the inputs
+let jumpDistance = String(readLine()!)!.components(separatedBy: " ").map({Int($0)!})[1]
+let clouds = String(readLine()!)!.components(separatedBy: " ").map({$0 == "1"}) // true for a thunder cloud, false otherwise
 
-// Declare global variables
-var output = [String : Stats]()
-let inputPath = FileManager.default.currentDirectoryPath + "/dev-challenge-data.csv"
-let calendar = Calendar.current
-let dateFormatter = DateFormatter()
-dateFormatter.dateFormat = "yyyy-MM-dd"
+// Process the problem
+var energy = 100
+var location = 0
+repeat {
+  location = (location + jumpDistance) % clouds.count
+  energy = (clouds[location]) ? energy - 3 : energy - 1
+} while (location != 0)
 
-// Attempt to open the CSV file and process the violations, one line at a time
-do {
-  let input = try String(contentsOfFile: inputPath).components(separatedBy: "\n")
-  for lineNumber in 1..<input.count {
-    let fields = input[lineNumber].components(separatedBy: ",")
-    let dateArray = fields[3].components(separatedBy: " ")[0].components(separatedBy: "-").map({Int($0)!})
-    let dateComponents = DateComponents(calendar: calendar, year: dateArray[0], month: dateArray[1], day: dateArray[2])
-    let dateOfViolation = calendar.date(from: dateComponents)!
-    if (output[fields[2]] != nil) {
-      output[fields[2]]!.count += 1
-      if (output[fields[2]]!.earliestOccurance > dateOfViolation) {
-        output[fields[2]]!.earliestOccurance = dateOfViolation
-      } else if (output[fields[2]]!.latestOccurance < dateOfViolation) {
-        output[fields[2]]!.latestOccurance = dateOfViolation
-      }
-    } else {
-      output[fields[2]] = Stats(count: 1, earliestOccurance: dateOfViolation, latestOccurance: dateOfViolation)
-    }
-  }
-} catch {
-  print("Sorry, something went wrong and we couldn't process your request")
-}
+// Print the remaining energy
+print(energy)
 
-// Print the output
-print("\n<<<<< Beginning of Report >>>>>\n")
-for violation in output {
-  print("Category: \(violation.key)")
-  print("\tNumber of Violations: \(violation.value.count)")
-  print("\tEarliest Violation: \(dateFormatter.string(from: violation.value.earliestOccurance))")
-  print("\tLatest Violation: \(dateFormatter.string(from: violation.value.latestOccurance))\n")
-}
-print("\n<<<<< End of Report >>>>>\n")
 
