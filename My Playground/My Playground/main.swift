041adcff84edb45d5736b1a04765ca506782e3e9
Marwan Alani <marwan@alani.ca>
2017-10-14 18:06:15 -0400

    Solved another problem for leetCode

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,19 +8,35 @@
 
 import Foundation
 
-func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-  var result = [Int]()
-  var map = [Int:Int]()
-  for (index, num) in nums.enumerated() {
-    if let complimentIndex = map[num] {
-      result.append(complimentIndex)
-      result.append(index)
-      break
-    } else {
-      map[target - num] = index
+func longestPalindrome(_ s: String) -> String {
+  let input: [Character] = Array(s.characters)
+  var begin = 0, end = 0
+  for i in 0..<input.count {
+    var b = i, e = i
+    // for odd
+    while (b >= 0 && e < input.count && input[b] == input [e]) {
+      b -= 1
+      e += 1
     }
-    print(index)
+    if (e - b - 1 > end - begin) {
+      begin = b + 1
+      end = e - 1
+    }
+    // for even
+    b = i
+    e = i + 1
+    while (b >= 0 && e < input.count && input[b] == input [e]) {
+      b -= 1
+      e += 1
+    }
+    if (e - b - 1 > end - begin) {
+      begin = b + 1
+      end = e - 1
+    }
+    if ((end - begin) / 2 > input.count - i) {
+      break
+    }
   }
-  return result
+  return String(input[begin...end])
 }
 
