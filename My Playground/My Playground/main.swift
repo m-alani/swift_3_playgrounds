0e5fd6c2caae8307ba1aeba6a7baf1a68762c161
Marwan Alani <marwan@alani.ca>
2017-10-28 13:36:38 -0400

    More leetCode practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,20 +8,37 @@
 
 import Foundation
 
-var result = [String]()
+// I, II, III, IV, V, VI, VII, VIII, IX, X
+// X, XX, XXX, XL, L, LX, LXX, LXXX, XC, C
+// C, CC, CCC, CD, D, DC, DCC, DCCC, CM, M
 
-func generateParenthesis(_ n: Int) -> [String] {
-  solveRecursive(n, 0, 0, "")
+func intToRoman(_ number: Int) -> String {
+  var result = ""
+  var num = number
+  while (num > 0) {
+    switch num {
+      case 1000..<4000: result.append("M"); num -= 1000
+      case 900..<1000: result.append("CM"); num -= 900
+      case 500..<900: result.append("D"); num -= 500
+      case 400..<500: result.append("CD"); num -= 400
+      case 100..<400: result.append("C"); num -= 100
+      case 90..<100: result.append("XC"); num -= 90
+      case 50..<90: result.append("L"); num -= 50
+      case 40..<50: result.append("XL"); num -= 40
+      case 10..<40: result.append("X"); num -= 10
+      case 9: result.append("IX"); num -= 9
+      case 5..<9: result.append("V"); num -= 5
+      case 4: result.append("IV"); num -= 4
+      case 1..<4: result.append("I"); num -= 1
+      default: num -= num
+    }
+  }
   return result
 }
 
-func solveRecursive(_ n: Int, _ left: Int, _ right: Int, _ currentStr: String) {
-  if (currentStr.count == n * 2) {
-    result.append(currentStr)
-    return
-  }
-  if (left < n) { solveRecursive(n, left + 1, right, currentStr + "(") }
-  if (right < left) { solveRecursive(n, left, right + 1, currentStr + ")") }
-}
 
-print(generateParenthesis(3))
+
+print(intToRoman(3927))
+
+
+
