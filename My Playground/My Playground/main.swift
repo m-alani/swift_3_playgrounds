2436d0ec74efb973ba92c02932a7042bc8b9bf0e
Marwan Alani <marwan@alani.ca>
2017-10-29 15:27:06 -0400

    leetCode practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -21,27 +21,13 @@ public class ListNode {
 }
 
 class Solution {
-  func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
-    
-    // Convert the input into an array (O(n))
-    var array = [Int]()
-    for list in lists {
-      var itr = list
-      while itr != nil {
-        array.append(itr!.val)
-        itr = itr?.next
-      }
+  func swapPairs(_ head: ListNode?) -> ListNode? {
+    if (head == nil || head?.next == nil) {
+      return head
     }
-    let dummy = ListNode(0)
-    var itr: ListNode? = dummy
-    
-    // Then sort the array (O(n log n)) and generate a new list of the merged numbers (O(n))
-    for num in array.sorted() {
-      itr?.next = ListNode(num)
-      itr = itr?.next
-    }
-    
-    // Total: O(n log n) time complexity & O(n) space complexity
-    return dummy.next
+    let swappedHead = head?.next
+    head?.next = swapPairs(head?.next?.next)
+    swappedHead?.next = head
+    return swappedHead
   }
 }
