d683b35f8ee5f31acd7266dc691715d611d18ca6
Marwan Alani <marwan@alani.ca>
2017-11-02 15:44:21 -0400

    More leetCode practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,15 +8,34 @@
 
 import Foundation
 
-func strStr(_ haystack: String, _ needle: String) -> Int {
-  if needle == "" { return 0 }
-  if needle.count > haystack.count { return -1 }
-  let range = haystack.range(of: needle, options: [.literal])
-  if let range = range {
-    return haystack.distance(from: haystack.startIndex, to: range.lowerBound)
-  } else {
-    return -1
-  }
+func nextPermutation(_ nums: inout [Int]) {
+    var l = -1, i = nums.count - 2
+    while (i >= 0) {
+        if nums[i] < nums[i + 1] {
+            l = i
+            break
+        }
+        i -= 1
+    }
+    if l == -1 {
+        nums = Array(nums.reversed())
+        return
+    }
+    var r = -1
+    i = nums.count - 1
+
+    while (i > l) {
+        if nums[i] > nums[l] {
+            r = i
+            break
+        }
+        i -= 1
+    }
+    nums.swapAt(l, r)
+    nums.replaceSubrange(l + 1..<nums.count, with: Array(Array(nums[l + 1...nums.count - 1]).reversed()))
 }
 
-print(strStr("abcdefghijklmn", "bc"))
+var sample = [1, 1, 5]
+nextPermutation(&sample)
+print(sample)
+
