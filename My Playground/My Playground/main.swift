04f5aa55042843e4c973a37b71aaddb6f23e56ac
Marwan Alani <marwan@alani.ca>
2017-11-11 15:43:16 -0500

    Practice Practice Practice!

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,31 +8,19 @@
 
 import Foundation
 
-var visited = [Bool]()
-var m = [[Int]]()
-
-func DFS(_ i: Int) {
-  for j in 0..<m.count {
-    if m[i][j] == 1 && !visited[j] {
-      visited[j] = true
-      DFS(j)
+func longestPalindrome(_ s: String) -> Int {
+  var length = 0
+  var itr = s.makeIterator()
+  var chars = Set<Character>()
+  while let char = itr.next() {
+    if chars.remove(char) != nil {
+      length += 2
+    } else {
+      chars.insert(char)
     }
   }
+  if chars.count > 0 { length += 1 }
+  return length
 }
 
-func findCircleNum(_ M: [[Int]]) -> Int {
-  var count = 0
-  visited = Array(repeating: false, count: M.count)
-  m = M
-  for i in 0..<m.count {
-    if !visited[i] {
-      DFS(i)
-      count += 1
-    }
-  }
-  return count
-}
-
-print(findCircleNum([[1,1,0],
-                     [1,1,0],
-                     [0,0,1]]))
+print(longestPalindrome("abccccdd"))
