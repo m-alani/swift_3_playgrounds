674cf53e4e67f76658976553ee452b427948b061
Marwan Alani <marwan@alani.ca>
2017-11-11 19:20:39 -0500

    Solved a hard one, BABY!

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,21 +8,21 @@
 
 import Foundation
 
-func getRow(_ rowIndex: Int) -> [Int] {
-  var row = Array(repeating: 0, count: rowIndex + 1)
-  var num = Double(rowIndex), den = 1.0, idx = 1
-  // Cover the edge case of index = 0
-  row[0] = 1
-  if rowIndex == 0 { return row }
-  // Find the values for the first half
-  while num >= den {
-    row[idx] = Int(Double(row[idx-1]) * num / den)
-    num -= 1; den += 1; idx += 1
+func longestConsecutive(_ nums: [Int]) -> Int {
+  var result = 0
+  var map = [Int: Int]()
+  for num in nums {
+    if map[num] == nil {
+      let left = map[num - 1] ?? 0
+      let right = map[num + 1] ?? 0
+      let current = left + 1 + right
+      result = max(current, result)
+      map[num] = current
+      map[num - left] = current
+      map[num + right] = current
+    }
   }
-  // Mirror the values to the second half
-  let half = row.count / 2
-  row[row.count-half...rowIndex] = ArraySlice(row[0..<half].reversed())
-  return row
+  return result
 }
 
-print(getRow(11))
+print(longestConsecutive([100, 4, 200, 1, 3, 2]))
