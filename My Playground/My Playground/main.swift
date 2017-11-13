a24a7a5296cb9b69ffa3a09406a99c047ff73d54
Marwan Alani <marwan@alani.ca>
2017-11-12 19:45:45 -0500

    Not gonna happen without DP :|

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -9,24 +9,18 @@
 import Foundation
 
 var result = false
-var longest = 0
 
 func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
-  var dict = Set<String>()
-  for word in wordDict {
-    if word.count > longest { longest = word.count }
-    dict.insert(word)
-  }
+  let dict = Set(wordDict)
   check(word: s, in: dict)
   return result
 }
 
 func check(word s: String, in words: Set<String>) {
-  if s.isEmpty { result = true; return }
+  if s.isEmpty || result { result = true; return }
   var subWord = ""
   for char in s {
     subWord.append(char)
-    if subWord.count > longest { return }
     if words.contains(subWord) {
       var newWord = s
       newWord.removeSubrange(s.range(of: subWord)!)
@@ -35,7 +29,8 @@ func check(word s: String, in words: Set<String>) {
   }
 }
 
-let testWord = "acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb"
-let testDict = ["abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd","a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab","aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d","cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda","ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"]
+let testWord = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+let testDict =
+["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
 
 print(wordBreak(testWord, testDict))
