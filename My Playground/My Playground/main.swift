0ea300a1ac6d5122b2141bc45c6daaf00470d6eb
Marwan Alani <marwan@alani.ca>
2017-11-12 19:33:14 -0500

    Working solution ... need to optimize!

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,15 +8,34 @@
 
 import Foundation
 
-func containsNearbyAlmostDuplicate(_ nums: [Int], _ k: Int, _ t: Int) -> Bool {
-  if nums.count < 2 || k < 1 { return false }
-  for i in 0..<nums.count-1 {
-    for j in i+1...i+k {
-      if j == nums.count { break }
-      else if abs(nums[i] - nums[j]) <= t { return true }
-    }
+var result = false
+var longest = 0
+
+func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+  var dict = Set<String>()
+  for word in wordDict {
+    if word.count > longest { longest = word.count }
+    dict.insert(word)
   }
-  return false
+  check(word: s, in: dict)
+  return result
 }
 
-print(containsNearbyAlmostDuplicate([2,2], 3, 0))
+func check(word s: String, in words: Set<String>) {
+  if s.isEmpty { result = true; return }
+  var subWord = ""
+  for char in s {
+    subWord.append(char)
+    if subWord.count > longest { return }
+    if words.contains(subWord) {
+      var newWord = s
+      newWord.removeSubrange(s.range(of: subWord)!)
+      check(word: newWord, in: words)
+    }
+  }
+}
+
+let testWord = "acaaaaabbbdbcccdcdaadcdccacbcccabbbbcdaaaaaadb"
+let testDict = ["abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd","a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab","aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d","cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda","ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"]
+
+print(wordBreak(testWord, testDict))
