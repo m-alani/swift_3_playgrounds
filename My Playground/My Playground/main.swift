3682b2e9a29a3fbf5338f372e22483f974f7f1e2
Marwan Alani <marwan@alani.ca>
2017-11-19 01:13:45 -0500

    HackerRank practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,25 +8,9 @@
 
 import Foundation
 
-func repeatedSubstringPattern(_ input: String) -> Bool {
-  guard input.count > 1 else { return false }
-  let s = input.utf8
-  var result = false
-  var length = s.count / 2
-  while length > 0 {
-    if s.count % length == 0 {
-      var start = s.startIndex, end = s.index(start, offsetBy: length)
-      while end != s.endIndex && s[start] == s[end] {
-        start = s.index(after: start); end = end == s.endIndex ? s.startIndex : s.index(after: end)
-      }
-      if end == s.endIndex {
-        length = 0
-        result = true
-      }
-    }
-    length -= 1
-  }
-  return result
+let input = UInt(readLine()!)!
+if input == 0 {
+  print("1")
+} else {
+  print(pow(2, Array(String(input, radix: 2)).filter({$0 == "0"}).count))
 }
-
-print(repeatedSubstringPattern("aabbaabb"))
