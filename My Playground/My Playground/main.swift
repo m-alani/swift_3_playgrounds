9c68832a2d397070f1304063d42e3bc9a1d96530
Marwan Alani <marwan@alani.ca>
2017-11-21 13:39:09 -0500

    leetCode Practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,24 +8,10 @@
 
 import Foundation
 
-func fourSumCount(_ A: [Int], _ B: [Int], _ C: [Int], _ D: [Int]) -> Int {
-  var count = 0
-  var dict = [Int:Int]()
-  for i in 0..<A.count {
-    for j in 0..<B.count {
-      let key = A[i] + B[j]
-      dict[key] = (dict[key] ?? 0) + 1
-    }
-  }
-  for i in 0..<C.count {
-    for j in 0..<D.count {
-      let key = (C[i] + D[j]) * -1
-      if let num = dict[key] {
-        count += num
-      }
-    }
-  }
-  return count
+func constructRectangle(_ area: Int) -> [Int] {
+    var width = Int(sqrt(Double(area)))
+    while area % width != 0 { width -= 1 }
+    return [area / width, width]
 }
 
-print(fourSumCount([-1,-1], [-1,1], [-1,1], [1,-1]))
+print(constructRectangle(25))
