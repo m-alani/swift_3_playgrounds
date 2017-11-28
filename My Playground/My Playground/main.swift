741a5d0feb109635c87134c2229f3a97fb25b328
Marwan Alani <marwan@alani.ca>
2017-11-28 16:48:50 -0500

    Task Scheduler. Not Working!

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,15 +8,28 @@
 
 import Foundation
 
-var test = [String: Int]()
-test["A"] = 16
-test["G"] = 11
-test["Z"] = 21
+// helper fucntion
+func dictionirize(_ arr: [Character]) -> [Int] {
+  var dict = [Character: Int]()
+  for char in arr {
+    dict[char] = (dict[char] ?? 0) + 1
+  }
+  return dict.values.sorted(by: >)
+}
 
-print(test)
+func leastInterval(_ tasks: [Character], _ n: Int) -> Int {
+  if tasks.count == 1 || n == 0 { return tasks.count }
+  var sortedTasks = dictionirize(tasks)
+  var cooldown = 0, total = 0, zeroCount = 0
+  while zeroCount != sortedTasks.count {
+    total += 1
+    let index = (cooldown + zeroCount) % sortedTasks.count
+    sortedTasks[index] -= 1
+    if sortedTasks[index] == 0 { zeroCount += 1 }
+    cooldown = (cooldown < n) ? cooldown + 1 : 0
+  }
+  return total
+}
 
-var sorted = test.sorted(by: { $0.value > $1.value })
+print(leastInterval(["A","A","A","C","B","B"], 2))
 
-sorted[0].value -= 1
-
-print(sorted)
