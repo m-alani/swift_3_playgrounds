5043e0492f7a2f58449ac0002535a38d2852ee85
Marwan Alani <marwan@alani.ca>
2017-11-27 19:25:30 -0500

    leetCode practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 
-// Task Scheduler
+func findShortestSubArray(_ nums: [Int]) -> Int {
+  var degree = 0, result = Int.max
+  var frequency = [Int:Int]()
+  var ranges = [Int: (first: Int, last: Int)]()
+  for (idx, num) in nums.enumerated() {
+    frequency[num] = (frequency[num] ?? 0) + 1
+    ranges[num] = ((ranges[num]?.first ?? idx), idx)
+    if (frequency[num]! > degree) { degree = frequency[num]! }
+  }
+  for num in nums {
+    if frequency[num] == degree {
+      result = min(result, ranges[num]!.last - ranges[num]!.first + 1)
+    }
+  }
+  return result
+}
 
-var test = [(task: String, freq: Int)]()
-test.append(("A", 3))
-test.append(("B", 6))
+print(findShortestSubArray([1,2,2,3,1]))
 
-print(test.sorted(by: { $0.freq > $1.freq }))
