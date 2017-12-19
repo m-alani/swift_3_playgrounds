195507abc3b37173f07f44e484b5f2742de02da0
Marwan Alani <marwan@alani.ca>
2017-12-19 14:43:15 -0500

    leetCode problem (Fibonacci spin off)

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -8,26 +8,17 @@
 
 import Foundation
 
-// That's the greedy solution ... slow AF
-func bulbSwitchLong(_ n: Int) -> Int {
-  if n < 2 { return n }
-  var bulbs = Array(repeating: true, count: n)
-  var i = 2
-  while i <= n {
-    var idx = i - 1
-    while idx < n {
-      bulbs[idx] = !bulbs[idx]
-      idx += i
-    }
+func climbStairs(_ n: Int) -> Int {
+  if n == 1 { return 1 }
+  if n == 2 { return 2 }
+  var one = 2, two = 1, i = 2, ways = 0
+  while i < n {
     i += 1
+    ways = one + two
+    two = one
+    one = ways
   }
-  return bulbs.filter({ $0 }).count
+  return ways
 }
 
-// This is an optimized solution, O(1)
-func bulbSwitch(_ n: Int) -> Int {
-  return Int(sqrt(Double(n)))
-}
-
-print(bulbSwitch(536271))
-
+print(climbStairs(5))
