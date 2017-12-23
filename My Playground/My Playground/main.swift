88998e3d7c526b27871c816b661026af1c3f36cb
Marwan Alani <marwan@alani.ca>
2017-12-23 16:57:21 -0500

    Daily exercise

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -2,13 +2,9 @@
 //  main.swift
 //  My Playground
 //
-//  Created by Marwan Alani on 2017-01-17.
-//  Copyright © 2017 Marwan Alani. All rights reserved.
-//
 
 import Foundation
 
-/* Fake Stack */
 var stack = [Int]()
 var max = Int.min
 
