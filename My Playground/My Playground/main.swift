4283dfb947bc5dcaf7261defa794503844760b77
Marwan Alani <marwan@alani.ca>
2017-12-25 17:50:34 -0500

    Playing with arc4random()

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,22 +6,6 @@
 
 import Foundation
 
-var stack = [Int]()
-var max = Int.min
-
-var n = Int(readLine()!)!
-for _ in 0..<n {
-  let inputStr = readLine()!
-  let input = inputStr.components(separatedBy: " ").map({Int($0)!})
-  if input[0] == 3 {
-    print(max)
-  } else if input[0] == 2 {
-    let temp = stack.removeLast()
-    if temp == max {
-      max = stack.max() ?? Int.min
-    }
-  } else {
-    stack.append(input[1])
-    if input[1] > max { max = input[1] }
-  }
+for _ in 0...10 {
+  print(Int(arc4random_uniform(10)) + 1)
 }
