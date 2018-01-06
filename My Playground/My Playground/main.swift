f0117509ed8d2ab31dd91977d29099fc4ad12d44
Marwan Alani <marwan@alani.ca>
2018-01-06 16:06:22 -0500

    leetCode practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,6 +6,19 @@
 
 import Foundation
 
-for _ in 0...10 {
-  print(Int(arc4random_uniform(10)) + 1)
+func pivotIndex(_ nums: [Int]) -> Int {
+  if nums.count >= 2 {
+    var sums = [Int]()
+    var sum = 0
+    sums.append(sum)
+    _ = nums.map({sum += $0; sums.append(sum)})
+    for (i, sum) in sums.enumerated() {
+      let right = (i == sums.count - 1) ? 0 : nums[i]
+      if sum == (sums.last! - sum - right) && (i != nums.count) { return i }
+    }
+  }
+  return -1
 }
+
+print(pivotIndex([-1,-1,-1,1,1,1]))
+
