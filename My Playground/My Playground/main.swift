1b778ef760910a1463d8ced1a7ef758d8561f362
Marwan Alani <marwan@alani.ca>
2018-01-12 13:15:24 -0500

    Playing around with readLine()

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,21 +6,14 @@
 
 import Foundation
 
-func fizzBuzz(_ n: Int) {
-  guard n > 0 else { return }
-  for i in 1...n {
-    var line = ""
-    switch i {
-    case _ where i % 3 == 0:
-      line += "Fizz"
-      if (i % 5 == 0) { fallthrough }
-    case _ where i % 5 == 0:
-      line += "Buzz"
-    default:
-      line += String(i)
-    }
-    print(line)
-  }
+var output = ""
+var input = ""
+var line = readLine(strippingNewline: false)
+
+while (line != nil) {
+  output.append(line!)
+  line = readLine(strippingNewline: false)
 }
 
-fizzBuzz(15)
+print("\n\(output)\n")
+
