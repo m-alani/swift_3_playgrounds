cef491562d1f86f0355cc35ddb173a7d904ef94a
Marwan Alani <marwan@alani.ca>
2018-01-28 17:21:22 -0500

    Getting back to practice things on leetCode

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,14 +6,17 @@
 
 import Foundation
 
-var output = ""
-var input = ""
-var line = readLine(strippingNewline: false)
-
-while (line != nil) {
-  output.append(line!)
-  line = readLine(strippingNewline: false)
+func fizzBuzz(_ n: Int) -> [String] {
+  var output = [String]()
+  guard n > 0 else { return output }
+  for i in 1...n {
+    var current = String()
+    if i % 3 == 0 { current.append("Fizz") }
+    if i % 5 == 0 { current.append("Buzz") }
+    if current.count == 0 { current.append(String(i)) }
+    output.append(current)
+  }
+  return output
 }
 
-print("\n\(output)\n")
-
+print(fizzBuzz(15))
