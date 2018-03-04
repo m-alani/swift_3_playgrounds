50893bcbd15b8674faff5bf99eaee8df3f53333f
Marwan Alani <marwan@alani.ca>
2018-03-03 19:06:50 -0500

    Some Switch/Case practice

--- Practice.playground/Pages/Global Functions and Methods.xcplaygroundpage/Contents.swift
+++ Practice.playground/Pages/Global Functions and Methods.xcplaygroundpage/Contents.swift
@@ -1,10 +1,19 @@
 import Foundation
 
-let auth = (name: "Marwan", pass: "password")
+let name: String? = "Marwan"
+let mail: String? = "marwan@alani.ca"
+let ip: String? = "127.0.0.1"
 
-switch auth {
-case ("Marwan", "password"):
-  print("Welcome, \(auth.name)")
+let user = (name, mail, ip)
+
+switch user {
+case let (name?, _, "127.0.0.1"?):
+  print("\(name) is connecting using localhost")
+case let (name?, mail?, _):
+  print("Hi \(name)")
+case let (nil, mail?, _):
+  print("Hi there!")
 default:
-  print("Access Denied!")
+  print("Acess Denied")
 }
+
