cbbd7e06c2fa9fbf77fe524eecb7119e5390ee95
Marwan Alani <marwan@alani.ca>
2018-03-04 15:22:11 -0500

    Expressive "if" statements

--- Practice.playground/Pages/Global Functions and Methods.xcplaygroundpage/Contents.swift
+++ Practice.playground/Pages/Global Functions and Methods.xcplaygroundpage/Contents.swift
@@ -1,19 +1,7 @@
 import Foundation
 
-let name: String? = "Marwan"
-let mail: String? = "marwan@alani.ca"
-let ip: String? = "127.0.0.1"
+let age = 19
 
-let user = (name, mail, ip)
-
-switch user {
-case let (name?, _, "127.0.0.1"?):
-  print("\(name) is connecting using localhost")
-case let (name?, mail?, _):
-  print("Hi \(name)")
-case let (nil, mail?, _):
-  print("Hi there!")
-default:
-  print("Acess Denied")
+if (18...65).contains(age) {
+  print("You should be working, you lazy ass!")
 }
-
