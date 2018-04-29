5f987089ea38d6cd028c6fac35ef106446f0d609
Marwan Alani <marwan@alani.ca>
2018-04-29 17:58:04 -0400

    Value swaps using tuples

--- Practice.playground/Pages/Playground.xcplaygroundpage/Contents.swift
+++ Practice.playground/Pages/Playground.xcplaygroundpage/Contents.swift
@@ -1,19 +1,7 @@
 //: ## Swift is 👑
 import Foundation
 
-enum DownloadTask {
-    case ready
-    case downloading(progress: Int)
-    case finished
-}
+var a = 5
+var b = 10
 
-let file1 = DownloadTask.downloading(progress: 0)
-
-switch file1 {
-case .downloading(let progress) where (1..<100).contains(progress):
-    print("Downloading")
-case .ready, .finished:
-    print("Not currently downloading")
-default:
-    print("Not sure what's goin on...")
-}
+(a, b) = (b ,a)
