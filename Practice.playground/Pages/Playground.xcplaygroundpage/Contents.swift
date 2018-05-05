89b72a92b7da342ed492f2a0714902d0e8d8549a
Marwan Alani <marwan@alani.ca>
2018-05-04 23:08:22 -0400

    Arrays performance

--- Practice.playground/Pages/Playground.xcplaygroundpage/Contents.swift
+++ Practice.playground/Pages/Playground.xcplaygroundpage/Contents.swift
@@ -1,12 +1,5 @@
 import Foundation
 
-var numbers = [28, -10, 11, 4, 99, 7, 0]
-
-let ascNumbers = numbers.sorted()
-let decNumbers = numbers.sorted(by: >)
-
-let subPositiveArrays = numbers.split(whereSeparator: {$0 < 0}) // Returns ArraySlice
-
 let arr1 = [Int](0...1000000)
 let arr2 = ContiguousArray<Int>(0...1000000)
 var start: CFAbsoluteTime, time: CFAbsoluteTime
@@ -14,9 +7,9 @@ var start: CFAbsoluteTime, time: CFAbsoluteTime
 start = CFAbsoluteTimeGetCurrent()
 _ = arr1.reduce(0, +)
 time = CFAbsoluteTimeGetCurrent() - start
-print("Regular array took ", String(format: "%.2f", time), " seconds")
+print("Regular array took ", String(format: "%.3f", time), " seconds")
 
 start = CFAbsoluteTimeGetCurrent()
 _ = arr2.reduce(0, +)
 time = CFAbsoluteTimeGetCurrent() - start
-print("Cont. array took ", String(format: "%.2f", time), " seconds")
+print("Cont. array took ", String(format: "%.3f", time), " seconds")
