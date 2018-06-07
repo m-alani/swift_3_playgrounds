d37088e6358f10bf8f5844d5ecb707b76a9df8a4
Marwan Alani <marwan@alani.ca>
2018-06-07 17:35:00 -0400

    leetCode Practice

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,13 +6,34 @@
 
 import Foundation
 
-func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-  var letters = [Character](magazine)
-  for letter in ransomNote {
-    guard let index = letters.index(of: letter) else { return false }
-    letters.remove(at: index)
+
+public class TreeNode {
+  public var val: Int
+  public var left: TreeNode?
+  public var right: TreeNode?
+  public init(_ val: Int) {
+    self.val = val
+    self.left = nil
+    self.right = nil
   }
-  return true
 }
 
-print(canConstruct("aa", "abba"))
+class Solution {
+  var sum = 0
+  
+  func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
+    traverse(root?.left, true)
+    traverse(root?.right, false)
+    return sum
+  }
+  
+  func traverse(_ node: TreeNode?, _ isLeftNode: Bool) {
+    guard let node = node else { return }
+    if node.left == nil && node.right == nil && isLeftNode {
+      self.sum += node.val
+    } else {
+      traverse(node.left, true)
+      traverse(node.right, false)
+    }
+  }
+}
