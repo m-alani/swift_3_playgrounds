72bb7fa7779afff7a821993e2dc9f2db8eba36fd
Marwan Alani <marwan@alani.ca>
2018-08-05 15:28:00 -0400

    Playing around with ranges

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,34 +6,6 @@
 
 import Foundation
 
+let fullName = "Anna Ostapenko"
 
-public class TreeNode {
-  public var val: Int
-  public var left: TreeNode?
-  public var right: TreeNode?
-  public init(_ val: Int) {
-    self.val = val
-    self.left = nil
-    self.right = nil
-  }
-}
-
-class Solution {
-  var sum = 0
-  
-  func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
-    traverse(root?.left, true)
-    traverse(root?.right, false)
-    return sum
-  }
-  
-  func traverse(_ node: TreeNode?, _ isLeftNode: Bool) {
-    guard let node = node else { return }
-    if node.left == nil && node.right == nil && isLeftNode {
-      self.sum += node.val
-    } else {
-      traverse(node.left, true)
-      traverse(node.right, false)
-    }
-  }
-}
+let validLength = (5...32).contains(fullName.count)
