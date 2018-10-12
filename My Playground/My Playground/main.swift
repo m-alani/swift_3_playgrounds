b94efa1d94385bb1516a7e2a491a1f920b0207a0
Marwan Alani <marwan@alani.ca>
2018-10-12 00:13:51 -0400

    Playing around with Binary Search

--- My Playground/My Playground/main.swift
+++ My Playground/My Playground/main.swift
@@ -6,11 +6,19 @@
 
 import Foundation
 
-func isValidResident(name: String) -> Bool {
-    return name != "Anna Ostapenko"
+func binarySearch(for number: Int, in numbers: [Int]) -> Int? {
+    guard !numbers.isEmpty else { return nil } // ensure we will not be searching in an empty array
+    var lowerBound = 0
+    var upperBound = numbers.count
+    while lowerBound < upperBound {
+        let index = lowerBound + (upperBound - lowerBound) / 2
+        if numbers[index] == number {
+            return index // we found the given number at this index
+        } else if numbers[index] < number {
+            lowerBound = index + 1
+        } else {
+            upperBound = index
+        }
+    }
+    return nil // the given number was not found
 }
-
-let fullName = "Anna Ostapenko"
-
-print("\(fullName) is allowed to stay in Canada: \(isValidResident(name: fullName))")
-
